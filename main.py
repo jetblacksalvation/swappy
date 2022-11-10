494690486c56470b750a0e4a44bebd78f5fd44d2
Xx_JetBlackSalvation_xX <91695842+jetblacksalvation@users.noreply.github.com>
2022-11-10 14:04:44 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from linkedlists import List
+
+
+l = List(0)
+l.append(1)
+l.append(2)
+l.append(3)
+l.append(4)
+l.append(5)
+l.append(6)
+l.append(7)
+
+l.print_list()
+print()
+l.swap_all()
+l.print_list()
